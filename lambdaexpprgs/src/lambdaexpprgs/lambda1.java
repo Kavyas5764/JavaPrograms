5f0401580c75788e663528974432c8cfcc7a1050
Kavyas5764 <94790756+Kavyas5764@users.noreply.github.com>
2021-12-17 10:01:48 +0530

    Add files via upload

---
+++ lambdaexpprgs/src/lambdaexpprgs/lambda1.java
@@ -0,0 +1,25 @@
+package lambdaexpprgs;
+import java.util.ArrayList;
+
+public class lambda1 {
+
+	public static void main(String[] args) {​​​​​
+
+		 ArrayList<String> list=new ArrayList<>();
+
+    	list.add("djch");
+
+    	list.add("djch");
+
+    	list.add("djch");
+
+    	list.add("djch");
+
+    	list.add("djch");
+    	list.forEach(
+            (String n)->System.out.println(n));
+		}
+	}​​​​​
+}
+
+}
